b8f6c93d0e4f548a8b75c5e776567600ce245c95
sha-255 <fawsgre@mail.ru>
2024-09-12 14:05:01 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'simple-parcking',
+  webDir: 'dist'
+};
+
+export default config;
