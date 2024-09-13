696eff441db3deef185b653ce42d7e5c089d7990
Smokee779 <vv_mamontenok@mail.ru>
2024-09-13 20:41:35 +0600

    ui auth

--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,10 +1,6 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Авторизация</ion-title>
-      </ion-toolbar>
-    </ion-header>
+
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
@@ -15,27 +11,29 @@
 
       <div id="container">
         <ion-header>Вход</ion-header>
-        <ion-input placeholder="Логин" v-model="signInData.login"></ion-input>
+        <ion-input placeholder="Логин" v-model="signInData.login" style="border:1px solid black !important; border-radius: 10px"></ion-input>
         <ion-input
           placeholder="Пароль"
-          v-model="signInData.password"
+          v-model="signInData.password" style="border:1px solid black !important; border-radius: 10px"
         ></ion-input>
-        <ion-button @click="signIn">Войти</ion-button>
-        <ion-header>Регистрация</ion-header>
-        <ion-input
+        <ion-button @click="signIn" style="margin-top: 10px;border-radius: 10px background-color: black!important; box-shadow: 0px 4px 8px 8px rgba(34, 60, 80, 0.2);
+">Войти</ion-button>
+        <ion-header style="margin-top: 40px; color:black!important; ">Регистрация</ion-header>
+        <ion-input style="border:1px solid black !important; border-radius: 10px"
           placeholder="Фамилия"
           v-model="registrateData.family"
         ></ion-input>
-        <ion-input placeholder="Имя" v-model="registrateData.name"></ion-input>
+        <ion-input placeholder="Имя" v-model="registrateData.name" style="border:1px solid black !important; border-radius: 10px"></ion-input>
         <ion-input
-          placeholder="Логин"
+          placeholder="Логин" style="border:1px solid black !important; border-radius: 10px"
           v-model="registrateData.login"
         ></ion-input>
         <ion-input
-          placeholder="Пароль"
+          placeholder="Пароль" style="border:1px solid black !important; border-radius: 10px"
           v-model="registrateData.password"
         ></ion-input>
-        <ion-button @click="registrate">Зарегистрироватся</ion-button>
+        <ion-button @click="registrate" style="margin-top:10px;box-shadow: 0px 4px 8px 8px rgba(34, 60, 80, 0.2);
+">Зарегистрироватся</ion-button>
       </div>
     </ion-content>
   </ion-page>
@@ -172,8 +170,34 @@ const registrate = async () => {
 
   margin: 0;
 }
-
+.input-wrapper .sc-ion-input-md{
+  border:1px solid black;
+}
+ion-header{
+  order: 0;
+}
 #container a {
   text-decoration: none;
+}
+ion-input{
+  background-color: white;
+  max-width: 400px;
+  margin-top: 10px;
+}
+ion-header{
+  background-color: #FFDD2D;
+  max-width: 200px;
+  border-radius: 10px;
+  padding: 5px;
+}
+#container{
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  flex-direction: column;
+  max-width: 600px;
+}
+.inner-scroll .scroll-y{
+  max-width: 100% !important;
 }
 </style>
